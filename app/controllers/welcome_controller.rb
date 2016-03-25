9a49c926a35c2e602e4a37e587731aaa2332456e
huuanh <huuanh90@gmail.com>
2016-03-25 10:50:55 +0700

    list and grid on welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
   def index
     @products = Product.paginate(:page => params[:page], :per_page => 10)
+    if params[:value] == 'list'
+      @mode = 'list'
+      render :list_grid
+    elsif params[:value] == 'grid'
+      @mode = 'grid'
+      render :grid_list
+    end
   end
 end
